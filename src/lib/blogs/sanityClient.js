f7bf8ef97271c3fc36bdb9ea840d3386684e83ac
jmeyu <eyujermin@gmail.com>
2023-08-29 22:19:00 +0800

    Updating Sanity Client ID

--- src/lib/blogs/sanityClient.js
+++ src/lib/blogs/sanityClient.js
@@ -1,7 +1,7 @@
 import sanityClient from '@sanity/client';
 
 const client = sanityClient({
-	projectId: 'wto9a8r0',
+	projectId: 'tzw45jpf',
 	dataset: 'production',
 	apiVersion: '2023-08-29',
 	useCdn: false // development
