34c726479b1da0325cb72583b5cada56931e4410
jmeyu <eyujermin@gmail.com>
2023-08-30 19:05:10 +0800

    Removing time reference on hero.

--- src/lib/portfolio/Hero_translations.js
+++ src/lib/portfolio/Hero_translations.js
@@ -2,11 +2,9 @@ export default {
   en: {
     "homepage.title": "Carpe diem.",
     "homepage.body": "By hook or by crook, I hope that you will possess yourselves of money enough to travel and to idle, to contemplate the future or the past of the world, to dream over books and loiter at street corners and let the line of thought dip deep into the stream. - Virginia Woolf",
-    "homepage.time": "Thanks for visiting on {{time}}.",
   },
   zh: {
     "homepage.title": "看点书、看点电影、品茗、小酌，过过日子。",
     "homepage.body": "我希望，无论大家通过什么的方法，都能挣到足够的钱，去旅行，去闲着，去思考世界的过去和未来，去看书做梦，去街角闲逛，让思绪钓线深深沉入街流之中。——伍尔夫​​",
-    "homepage.time": "{{time}}，这一天，你来到了这里。",
   },
 };
