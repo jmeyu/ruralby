71fd3f78ef07d7a69b496bfb219dca005101c13f
jmeyu <eyujermin@gmail.com>
2023-08-30 18:28:18 +0800

    Updating chinese word and content

--- src/lib/portfolio/Hero_translations.js
+++ src/lib/portfolio/Hero_translations.js
@@ -1,12 +1,12 @@
 export default {
   en: {
     "homepage.title": "Curling up with a good book, indulging in captivating movies, savoring a cup of tea, and toasting to life's moments.",
-    "homepage.body": "By hook or by crook, I hope that you will possess yourselves of money enough to travel and to idle, to contemplate the future or the past of the world, to dream over books and loiter at street corners and let the line of thought dip deep into the stream.",
+    "homepage.body": "By hook or by crook, I hope that you will possess yourselves of money enough to travel and to idle, to contemplate the future or the past of the world, to dream over books and loiter at street corners and let the line of thought dip deep into the stream. - Virginia Woolf",
     "homepage.time": "Thanks for visiting on {{time}}.",
   },
   zh: {
     "homepage.title": "看点书、看点电影、品茗、小酌，过过日子。",
-    "homepage.body": "如电影《死亡诗社》所述：我步入丛林／因为我希望生活得有意义／我希望活得深刻／吸取生命中所有的精华／把非生命的一切都击溃／以免当我生命终结／发现自己从没有活过。",
+    "homepage.body": "我希望，无论大家通过什么的方法，都能挣到足够的钱，去旅行，去闲着，去思考世界的过去和未来，去看书做梦，去街角闲逛，让思绪钓线深深沉入街流之中。——伍尔夫​​",
     "homepage.time": "{{time}}，这一天，你来到了这里。",
   },
 };
