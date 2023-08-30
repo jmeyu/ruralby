7c04e166eef653eeefdd292ac60a086314365f38
jmeyu <eyujermin@gmail.com>
2023-08-30 19:56:02 +0800

    Updating header GA tag

--- src/lib/blogs/Header.svelte
+++ src/lib/blogs/Header.svelte
@@ -32,12 +32,6 @@
 			}
 		}
 	}
-
-	window.dataLayer = window.dataLayer || [];
-	function gtag(){dataLayer.push(arguments);}
-	gtag('js', new Date());
-  
-	gtag('config', 'G-JPRYYE84RE');
 </script>
 
 <svelte:window
