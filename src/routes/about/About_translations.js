85ea43062d38e9af7c1df52faa3a28b406225a08
jmeyu <eyujermin@gmail.com>
2023-09-11 18:09:48 +0800

    Adding translation to the About page

---
+++ src/routes/about/About_translations.js
@@ -0,0 +1,10 @@
+export default {
+  en: {
+    "homepage.title": "Acknowledgement",
+    "homepage.body": "As the heads up",
+  },
+  zh: {
+    "homepage.title": "特此声明",
+    "homepage.body": "特此声明一下吼​​",
+  },
+};
