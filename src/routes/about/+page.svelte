ad53c35e1248664be9f8f7645147f6d3a80285e6
jmeyu <eyujermin@gmail.com>
2023-10-04 22:06:54 +0800

    Moving translation button around

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -15,6 +15,7 @@
 </script>
 
 <main>
+	<h1>{$t("about.title")}</h1>
 	<p align="right">
 		<svg 
 			class="localeSwitcher"
@@ -33,7 +34,6 @@
 			</g>
 		</svg>
 	</p>
-	<h1>{$t("about.title")}</h1>
 	<hr />
 	<img
 		src="https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExNzc4NWMzMjRhYWQzOTNiOTE2ZGYzOTc5ZGVlZDhmOTIxYjM2NGI2MCZjdD1n/ASd0Ukj0y3qMM/giphy.gif"
