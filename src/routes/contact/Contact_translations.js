5ed58234965199182faef794cd4dacaa49560a94
jmeyu <eyujermin@gmail.com>
2023-10-04 23:06:02 +0800

    Updating translation for Contact

---
+++ src/routes/contact/Contact_translations.js
@@ -0,0 +1,22 @@
+export default {
+  en: {
+    "contact.header": "Say Hello Secretly!",
+    "contact.disclaimer": "Your email, name and message will not be published.",
+    "contact.name": "Your name",
+    "contact.annonymous": "Annonymous",
+    "contact.email": "Email Address",
+    "contact.message": "Message",
+    "contact.messageBody": "We've known each other for so long, your heart's been aching, but you're too shy to say it...",
+    "contact.send":"SEND MESSAGE",
+  },
+  zh: {
+    "contact.header": "一个匿名投信站。",
+    "contact.disclaimer": "你的邮箱、名字与信件内容将不会被公开。",
+    "contact.name": "名字/称呼",
+    "contact.annonymous": "例子：漂亮的英雄、无业游民、王大壮...",
+    "contact.email": "邮箱",
+    "contact.message": "内容",
+    "contact.messageBody": "你想说……",
+    "contact.send":"发送",
+  },
+};
