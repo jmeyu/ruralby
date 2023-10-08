96555c143b9b57835cd95abcce8aae1eb27a145e
jmeyu <eyujermin@gmail.com>
2023-10-08 13:26:28 +0800

    Updating tags

---
+++ src/lib/analytics.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+    import { page } from '$app/stores'
+  
+    $: {
+      if (typeof gtag !== 'undefined') {
+        gtag('config', 'MEASUREMENT_ID', {
+          page_title: document.title,
+          page_path: $page.url.pathname,
+        })
+      }
+    }
+  </script>
+  
+  <svelte:head>
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-LB9Y644193"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-LB9Y644193');
+    </script>
+  </svelte:head>
+  
